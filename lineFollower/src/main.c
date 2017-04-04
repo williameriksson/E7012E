b23a57e12110aebc9db148ca05acc4913d19d303
William Eriksson <William Eriksson>
2017-04-04 13:04:19 +0200

    Initial project commit

---
+++ lineFollower/src/main.c
@@ -0,0 +1,21 @@
+#include "stm32f4xx.h"
+
+
+int main(void)
+{
+  /**
+  *  IMPORTANT NOTE!
+  *  The symbol VECT_TAB_SRAM needs to be defined when building the project
+  *  if code has been located to RAM and interrupts are used. 
+  *  Otherwise the interrupt table located in flash will be used.
+  *  See also the <system_*.c> file and how the SystemInit() function updates 
+  *  SCB->VTOR register.  
+  *  E.g.  SCB->VTOR = 0x20000000;  
+  */
+
+
+  /* Infinite loop */
+  while (1) {
+
+  }
+}
