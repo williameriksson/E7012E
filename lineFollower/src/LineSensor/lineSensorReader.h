7e0fb31511ff3a9cb1508dfa857773cc037f4937
Sebastian Emmot <sebastian.emmot@gmail.com>
2017-04-19 15:59:04 +0200

    Linesensor update, logic almost done.

--- lineFollower/src/LineSensor/lineSensorReader.h
+++ lineFollower/src/LineSensor/lineSensorReader.h
@@ -1,21 +1,16 @@
-/*
- * lineSensorReader.h
- *
- *  Created on: 11 Apr 2017
- *      Author: sebas
- */
-
 #ifndef LINESENSORREADER_H_
 #define LINESENSORREADER_H_
 #define LINESENSORARRAY_SIZE 9
 
 #include "stm32f4xx.h"
+#include "math.h"
 
 typedef struct {
 	uint8_t values[LINESENSORARRAY_SIZE];
-} LineSensors;
+} LineSensorArray;
 
 void initLineSensorReader(void);
-void updateLineSensors(void);
+void updateLineSensorArray(LineSensorArray*);
+float updateDistanceOffset(LineSensorArray*);
 
 #endif /* LINESENSORREADER_H_ */
